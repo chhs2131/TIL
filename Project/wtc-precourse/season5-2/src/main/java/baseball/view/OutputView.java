e552cf1dfae50d389ca98d1c9f46f7cd379d525c
Basak <chgy2131@naver.com>
2022-11-28 21:11:42 +0900

    feat(OutputView): 문구 출력하기
    - 게임시작 문구 출력하기
    - 사용자가 숫자 입력시 안내 문구 출력
    - 입력한 수에 대한 결과를 볼, 스트라이크 개수로 표시
    -
    게임 종료 이후 더 할건지 안내 문구 출력하기

--- Project/wtc-precourse/season5-2/src/main/java/baseball/view/OutputView.java
+++ Project/wtc-precourse/season5-2/src/main/java/baseball/view/OutputView.java
@@ -1,12 +1,35 @@
 package baseball.view;
 
+import baseball.domain.GameResult;
+
 public class OutputView {
+    private static final int NUMBERS_SIZE = 3;
     public void printWelcome() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
     public void printNumbersInputGuide() {
+        System.out.print("숫자를 입력해주세요 : ");
     }
-    public void printGameResult() {
+    public void printGameResult(GameResult gameResult) {
+        String sentence = gameResultSentence(gameResult);
+        System.out.println(sentence);
+    }
+    private String gameResultSentence(GameResult gameResult) {
+        if (gameResult.getStrike() == 3) {
+            return "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+        }
+        if (gameResult.getStrike() != 0 && gameResult.getBall() != 0) {
+            return gameResult.getBall() + "볼 " + gameResult.getStrike() + "스트라이크";
+        }
+        if (gameResult.getStrike() != 0) {
+            return gameResult.getStrike() + "스트라이크";
+        }
+        if (gameResult.getBall() != 0) {
+            return gameResult.getBall() + "볼";
+        }
+        return "낫싱";
     }
     public void printContinueGuide() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
