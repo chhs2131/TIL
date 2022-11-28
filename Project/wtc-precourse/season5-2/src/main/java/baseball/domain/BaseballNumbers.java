043239ae4a228acf6bcc600903b0c8dd3c57c65b
Basak <chgy2131@naver.com>
2022-11-28 17:57:10 +0900

    feat(BaseballNumbers): 야구게임숫자를 검증하는 기능 추가
    - 숫자의 갯수가 3개인지 확인
    - 숫자가 범위(1~9) 내에 있는지 확인
    - 숫자가 서로 중복되지 않는지 확인

--- Project/wtc-precourse/season5-2/src/main/java/baseball/domain/BaseballNumbers.java
+++ Project/wtc-precourse/season5-2/src/main/java/baseball/domain/BaseballNumbers.java
@@ -1,22 +1,41 @@
 package baseball.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BaseballNumbers {
+    private static final int NUMBERS_SIZE = 3;
+    private static final int NUMBER_RANGE_MIN = 1;
+    private static final int NUMBER_RANGE_MAX = 9;
     private final List<Integer> numbers;
 
     public BaseballNumbers(List<Integer> numbers) {
+        validate(numbers);
         this.numbers = numbers;
     }
-    private void validate() {
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
     }
-    private void validateNumber() {
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != NUMBERS_SIZE) {
+            throw new IllegalArgumentException("[ERROR] wrong size");
+        }
     }
-    private void validateSize() {
+    private void validateRange(List<Integer> numbers) {
+        for (int n : numbers) {
+            if (n < NUMBER_RANGE_MIN || NUMBER_RANGE_MAX < n) {
+                throw new IllegalArgumentException("[ERROR] out of range");
+            }
+        }
     }
-    private void validateRange() {
-    }
-    private void validateDuplicate() {
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> withoutDuplicate = new HashSet<>(numbers);
+        if (withoutDuplicate.size() != NUMBERS_SIZE) {
+            throw new IllegalArgumentException("[ERROR] duplicate number");
+        }
     }
 
     public boolean compare() {
