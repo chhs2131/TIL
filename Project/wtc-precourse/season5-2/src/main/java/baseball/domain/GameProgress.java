e1b9694c7b91c016f2f0fe39f2d35b100346a0fc
Basak <chgy2131@naver.com>
2022-11-29 00:31:12 +0900

    feat(GameProgress): 현재 상태가 승리인지 확인하는 기능 추가

--- Project/wtc-precourse/season5-2/src/main/java/baseball/domain/GameProgress.java
+++ Project/wtc-precourse/season5-2/src/main/java/baseball/domain/GameProgress.java
@@ -2,4 +2,11 @@ package baseball.domain;
 
 public enum GameProgress {
     PLAYING, WINNING;
+
+    public static boolean isWinning(final GameProgress progress) {
+        if (progress == WINNING) {
+            return true;
+        }
+        return false;
+    }
 }
