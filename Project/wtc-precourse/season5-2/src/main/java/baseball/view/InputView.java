1ab45f7a492e422060bd0438f920d9900013d7cf
Basak <chgy2131@naver.com>
2022-11-28 21:17:02 +0900

    feat(InputView): 입력받은 문자열이 숫자들인지 검증하는 기능 추가

--- Project/wtc-precourse/season5-2/src/main/java/baseball/view/InputView.java
+++ Project/wtc-precourse/season5-2/src/main/java/baseball/view/InputView.java
@@ -1,8 +1,27 @@
 package baseball.view;
 
+import baseball.domain.BaseballNumbers;
+import baseball.domain.GameCommand;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class InputView {
-    public void readBaseballNumbers() {
+    private static final String ALL_NUMBERS = "^[0-9]+$";
+    public BaseballNumbers readBaseballNumbers() {
+        String input = Console.readLine();
     }
-    public void readGameCommand() {
+
+    private List<Integer> stringToIntegerList(String input) {
+
+    }
+
+    private void validateStringNumber(String number) {
+        if (!number.matches(ALL_NUMBERS)) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값이 입력되었습니다.");
+        }
+    }
+    public GameCommand readGameCommand() {
+        return GameCommand.RESTART;
     }
 }
