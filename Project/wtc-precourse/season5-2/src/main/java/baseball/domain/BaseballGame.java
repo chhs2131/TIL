dd481773ee87874c9e27a4d06dda0441242084c8
Basak <chgy2131@naver.com>
2022-11-28 22:56:05 +0900

    feat(BaseballGame): 정답으로 사용할 랜덤 번호를 생성하는 기능 추가

--- Project/wtc-precourse/season5-2/src/main/java/baseball/domain/BaseballGame.java
+++ Project/wtc-precourse/season5-2/src/main/java/baseball/domain/BaseballGame.java
@@ -1,7 +1,23 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGame {
-    public void makeRandomNumbers() {
+    private static final int NUMBERS_SIZE = 3;
+    private static final int NUMBER_RANGE_MIN = 1;
+    private static final int NUMBER_RANGE_MAX = 9;
+    public BaseballNumbers makeRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(NUMBER_RANGE_MIN, NUMBER_RANGE_MAX);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return new BaseballNumbers(computer);
     }
     public void compareNumbers() {
     }
