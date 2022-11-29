06ecc92227dbe42d1bfc0e3af2eff2340eb7034a
Basak <chgy2131@naver.com>
2022-11-29 21:03:44 +0900

    feat(RacingGame4-2,Car): 출전자의 이름을 통해 Car를 생성하고, 이름의 글자수를 검증하는 기능 추가

--- Project/wtc-precourse/season4-2/src/main/java/racingcar/domain/Car.java
+++ Project/wtc-precourse/season4-2/src/main/java/racingcar/domain/Car.java
@@ -1,11 +1,19 @@
 package racingcar.domain;
 
 public class Car {
+    private static final int MAXIMUM_NAME_LENGTH = 5;
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        validateNameLength(name);
         this.name = name;
+    }
+
+    private static void validateNameLength(String name) {
+        if (name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 5글자까지만 사용할 수 있습니다. (" + name + ", " + name.length() + "글자)");
+        }
     }
 
     public void move() {
