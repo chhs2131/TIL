44c8ca185bb294a4a20f04087e530c997e3dd1bb
Basak <chgy2131@naver.com>
2022-11-29 23:09:58 +0900

    feat(RacingGame4-2,OutputView): 출력 문구 지정

--- Project/wtc-precourse/season4-2/src/main/java/racingcar/view/OutputView.java
+++ Project/wtc-precourse/season4-2/src/main/java/racingcar/view/OutputView.java
@@ -1,20 +1,36 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
 import java.util.List;
 
 public class OutputView {
     public static void printInputCarNameGuide() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
     public static void printInputDestinationGuide() {
+        System.out.println("시도할 회수는 몇회인가요?");
     }
 
     public static void printRacingResult() {
+        System.out.println("실행 결과");
+    }
+
+    private static void printCarInformation(Car car) {
+        System.out.print(car.getName() + " : ");
+        for (int i = 0; i < car.getPosition(); i++) {
+            System.out.print("-");
+        }
+        System.out.println("");
     }
 
     public static void printWinner(List<String> winners) {
+        String sentence = String.join(", ", winners);
+        System.out.println("최종 우승자 : " + sentence);
     }
 
-    public static void printException() {
+    public static void printException(String e) {
+        System.out.println("[ERROR] " + e);
     }
 }
