67ff32b24c45a62697d886d24b963d0d94dcbca6
Basak <chgy2131@naver.com>
2022-12-08 22:02:19 +0900

    feat(InputView,OutputView): View 뼈대 작성

--- Project/wtc-precourse/season5-3/src/main/java/lotto/view/InputView.java
+++ Project/wtc-precourse/season5-3/src/main/java/lotto/view/InputView.java
@@ -1,4 +1,19 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static int readPrice() {
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
+
+    public static void readWinnerNumbers() {
+        String input = Console.readLine();
+    }
+
+    public static int readBounusNumber() {
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
 }
--- Project/wtc-precourse/season5-3/src/main/java/lotto/view/OutputView.java
+++ Project/wtc-precourse/season5-3/src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,24 @@
 package lotto.view;
 
 public class OutputView {
+    public static void printInputPriceGuide() {
+        System.out.println("구매금액을 입력해 주세요.");
+    }
+
+    public static void printMyLotto() {
+        System.out.println("개를 구매했습니다.");
+    }
+
+    public static void printInputWinnerNumbersGuide() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void printInputBonusNumberGuide() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    public static void printLottoResult() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
 }
